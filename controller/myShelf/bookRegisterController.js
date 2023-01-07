8ca637243dbd86fce54c1b45566200f0bcc14bc9
bik1111 <bik1111@khu.ac.kr>
2023-01-07 22:53:21 +0900

    upload project

--- controller/myShelf/bookRegisterController.js
+++ controller/myShelf/bookRegisterController.js
@@ -4,15 +4,8 @@ const bookService = require("../../service/myShelf/bookRegisterService");
 module.exports.registerBooks = async (req, res) => {
     try {
         const { user_id } =req.params; 
-        const title = req.body.title;
-        const author = req.body.author;
-        const publisher = req.body.publisher;
-        const img_url = req.body.img_url;
-        //var status = req.body.status;
-        // const title = parseInt(req.params.title);
-        // var total_reading_time = parseInt(req.body.total_reading_time);
-        //const register = parseInt(req.params.register);
-        //const status = parseInt(req.params.status);
+        const { title, author, publisher, img_url }  = req.body;
+  
         const registerResponse = await bookService.registerBook(
             user_id,
             title,
@@ -20,7 +13,9 @@ module.exports.registerBooks = async (req, res) => {
             publisher,
             img_url,
         );
-        res.send("책 등록 완료");
+        console.log(registerResponse);
+        return res.send(registerResponse);
+      
       }
     catch (err) {
       console.log("Error", err);
